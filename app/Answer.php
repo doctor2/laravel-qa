80b81195426a7fcdddf01b6117490b40351fc881
Ivan <doctor254452009@ya.ru>
2018-11-01 18:58:41 +0500

    update best_answer_id

--- app/Answer.php
+++ app/Answer.php
@@ -32,7 +32,13 @@ class Answer extends Model
             // $answer->question->save();
         });
         static::deleted(function($answer){
-            $answer->question->decrement('answers_count');
+            $question = $answer->question;
+            $question->decrement('answers_count');
+            if($question->best_answer_id === $answer->id)
+            {
+                $question->best_answer_id = null;
+                $question->save();
+            }
         });
 
         // static::saved(function(){
@@ -43,5 +49,10 @@ class Answer extends Model
     public function getCreatedDateAttribute()
     {
     	return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted': '';
     }
 }
--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -9,38 +9,49 @@
                     @include('layouts._message')
                     @foreach ($answers as $answer)
                         <div class="media">
+                            <div class="d-flex flex-column">
+                                <a href="" title="This answer is usful" class="vote-up"> vote up</a>
+                                <span class="votu-count">123</span>
+                                <a href="" title="This answer is not usful" class="vote-down">vote down</a>
+                            <a href="Click as favorite" class="favorite {{$answer->status}}">favorite 
+                                    <span class="favorites-count">12</span>
+                                </a>
+                            </div>
                             <div class="media-body">
                                 {!!$answer->body_html!!}
-                                <div class="col-4">
-                                    <div class="ml-auto">
-                                        @can('update', $answer)
-                                            <a href="{{route('questions.answers.edit',[$questionId, $answer->id])}}" 
-                                            class="btn btn-small btn-outline-info"> Edit question</a>
-                                        @endif
-                                        @can('delete', $answer)
-                                            <form action="{{route('questions.answers.destroy',[$questionId, $answer->id])}}" method="post">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-small btn-outline-danger"
-                                                onclick="return confirm('Are you sure?')"
-                                                >delete</button>
-                                            </form>
-                                        @endcan
-                                    </div>
+                                <div class="row">
+                                        <div class="col-4">
+                                                <div class="ml-auto">
+                                                    @can('update', $answer)
+                                                        <a href="{{route('questions.answers.edit',[$questionId, $answer->id])}}" 
+                                                        class="btn btn-small btn-outline-info"> Edit question</a>
+                                                    @endif
+                                                    @can('delete', $answer)
+                                                        <form action="{{route('questions.answers.destroy',[$questionId, $answer->id])}}" method="post">
+                                                            @csrf
+                                                            @method('DELETE')
+                                                            <button type="submit" class="btn btn-small btn-outline-danger"
+                                                            onclick="return confirm('Are you sure?')"
+                                                            >delete</button>
+                                                        </form>
+                                                    @endcan
+                                                </div>
+                                            </div>
+                                            <div class="col-4">
+                                            </div>
+                                            <div class="col-4">
+                                                <span class="text-muted"> {{$answer->created_date}}</span>
+                                                <div class="media">
+                                                    <a href="{{ $answer->user->url}}" class="pr-2">
+                                                        <img src="{{$answer->user->avatar}}" alt="">
+                                                    </a>
+                                                    <div class="media-body">
+                                                    <a href="{{$answer->user->url}}"> {{$answer->user->name}}</a>
+                                                    </div>
+                                                </div>
+                                            </div>
                                 </div>
-                                <div class="col-4">
-                                </div>
-                                <div class="col-4">
-                                    <span class="text-muted"> {{$answer->created_date}}</span>
-                                    <div class="media">
-                                        <a href="{{ $answer->user->url}}" class="pr-2">
-                                            <img src="{{$answer->user->avatar}}" alt="">
-                                        </a>
-                                        <div class="media-body">
-                                        <a href="{{$answer->user->url}}"> {{$answer->user->name}}</a>
-                                        </div>
-                                    </div>
-                                </div>
+                                
                                
                             </div>
                         </div> 
