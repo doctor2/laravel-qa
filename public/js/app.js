01d9ae68670f53a950078b3eb9fd91528d9f4186
Ivan <doctor254452009@ya.ru>
2019-04-08 18:47:23 +0500

    implement deletion event

--- public/js/app.js
+++ public/js/app.js
@@ -50497,6 +50497,10 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
     },
 
     methods: {
+        remove: function remove(index) {
+            this.answers.splice(index, 1);
+            this.count--;
+        },
         fetch: function fetch(endpoint) {
             var _this = this;
 
@@ -50607,7 +50611,7 @@ exports = module.exports = __webpack_require__(4)(false);
 
 
 // module
-exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
+exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
 
 // exports
 
@@ -50703,9 +50707,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
                 position: 'center',
                 buttons: [['<button><b>YES</b></button>', function (instance, toast) {
                     axios.delete(_this2.endpoint).then(function (res) {
-                        $(_this2.$el).fadeOut(500, function () {
-                            _this2.$toast.success(res.data.message, 'Success', { timeout: 3000 });
-                        });
+                        _this2.$emit('deleted');
                     });
                     instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
                 }, true], ['<button>NO</button>', function (instance, toast) {
@@ -50886,10 +50888,15 @@ var render = function() {
                 _vm._v(" "),
                 _c("hr"),
                 _vm._v(" "),
-                _vm._l(_vm.answers, function(answer) {
+                _vm._l(_vm.answers, function(answer, index) {
                   return _c("answer", {
                     key: answer.id,
-                    attrs: { answer: answer }
+                    attrs: { answer: answer },
+                    on: {
+                      deleted: function($event) {
+                        _vm.remove(index)
+                      }
+                    }
                   })
                 }),
                 _vm._v(" "),
--- resources/js/components/Answers.vue
+++ resources/js/components/Answers.vue
@@ -8,7 +8,7 @@
                     </div>
                     <hr>
 
-                    <answer v-for="answer in answers" :answer="answer" :key="answer.id">
+                    <answer @deleted="remove(index)" v-for="(answer, index) in answers" :answer="answer" :key="answer.id">
                     </answer>
 
                     <div class="text-center mt-3" v-if="nextUrl" >
@@ -37,6 +37,10 @@
             this.fetch(`/questions/${this.questionId}/answers`);
         },
         methods:{
+            remove (index) {
+                this.answers.splice(index, 1);
+                this.count--;
+            },
             fetch(endpoint){
                 axios.get(endpoint)
                     .then(({data}) => {
